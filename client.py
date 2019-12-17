c85aa04a2efc0096551c46793239d3db7b86fa21
RustyGuard <44666045+RustyGuard@users.noreply.github.com>
2019-12-17 16:47:49 +0400

    Basic PyGame example

--- client.py
+++ client.py
@@ -1,47 +1,114 @@
 import socket
 import threading
+from threading import Lock
+
+import pygame
+from pygame.sprite import Group
 
 
 class Client:
-    def __init__(self, name):
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__(self, name, listener):
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = "localhost"
         self.port = 5556
         self.addr = (self.server, self.port)
         self.name = name
         self.i = 0
         self.connect()
+        self.listener = listener
+
+    def start_thread(self):
         thread = threading.Thread(target=self.thread_listen)
         thread.start()
 
     def connect(self):
         try:
-            self.client.connect(self.addr)
-            self.client.sendall(self.name.encode())
+            self.conn.connect(self.addr)
+            self.conn.sendall(self.name.encode())
         except Exception as e:
             self.disconnect(e)
 
     def send(self, data):
         try:
-            self.client.send(data.encode())
+            self.conn.send(data.encode())
         except socket.error as e:
             self.disconnect(e)
 
     def disconnect(self, msg):
         print("[EXCEPTION] Disconnected from server:", msg)
-        self.client.close()
+        self.conn.close()
 
     def thread_listen(self):
         while True:
-            msg = self.client.recv(1024)
-            print('taken command', msg)
-            if msg.startswith('0'.encode()):
-                self.i = self.i + 1
-            elif msg.startswith('1'.encode()):
-                print('I is:', self.i)
+            try:
+                command = self.conn.recv(1024)
+                self.listener(command.decode())
+            except Exception as ex:
+                print('[READ THREAD]', ex)
+                print('[READ THREAD] NO LONGER READING FROM SERVER!')
+                return
 
 
-if __name__ == '__main__':
-    n = Client(input())
-    while True:
-        n.send(input())
+class Bomb(pygame.sprite.Sprite):
+    bomb = pygame.image.load('sprites/bomb.png')
+
+    def __init__(self, group, x, y):
+        self.image = Bomb.bomb
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        super().__init__(group)
+
+
+class Game:
+    def __init__(self):
+        self.sprites = Group()
+        self.lock = Lock()
+
+    def drawSprites(self, surface):
+        self.lock.acquire()
+        self.sprites.draw(surface)
+        self.lock.release()
+
+    def addSprite(self, x, y):
+        self.lock.acquire()
+        Bomb(self.sprites, x, y)
+        self.lock.release()
+
+
+def listen(command):
+    print(command)
+    # 0 - Game Started
+    # 1 - Add object at [x, y]
+    if command.startswith('1'):
+        x, y = list(map(int, command[2::].split()))
+        game.addSprite(x, y)
+    else:
+        print('Taken message:', command)
+
+
+# Init
+client = Client('test', listen)
+while client.conn.recv(1024).decode() != '0':
+    pass
+pygame.init()
+size = 320, 470
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+running = True
+game = Game()
+
+client.start_thread()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            game.addSprite(event.pos[0] - 50, event.pos[1] - 50)
+            client.send(f'0 1 {event.pos[0] - 50} {event.pos[1] - 50}')
+    screen.fill((0, 0, 0))
+    game.drawSprites(screen)
+
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 import threading
 
+NEED_PLAYERS = 3
+
 
 class ClienConnection:
     def __init__(self, addr, name, conn):
@@ -14,10 +16,11 @@ class ClienConnection:
 class Server:
     def __init__(self):
         self.clients = []
+        self.connected = 0
+        self.waiting = True
 
     def send_all(self, msg):
         for c in self.clients:
-            print(c)
             c.send(msg)
 
     def thread_connection(self):
@@ -32,12 +35,14 @@ class Server:
         s.listen(1)
         print("Waiting for a connection, Server Started")
 
-        while True:
+        while self.connected < NEED_PLAYERS:
             print("[CONNECT] Finding connection!")
             conn, addr = s.accept()
-            print("[CONNECT] New connection!")
-
+            self.connected += 1
+            print(f"[CONNECT] Connected [{self.connected}/{NEED_PLAYERS}]!")
             self.authentication(conn, addr)
+        self.send_all('0')
+        self.waiting = False
 
     def authentication(self, conn, addr):
         try:
@@ -50,26 +55,35 @@ class Server:
             self.clients.append(client)
             thread = threading.Thread(target=self.player_input_thread, args=(client, ))
             thread.start()
+            print(f"[AUTHENTICATION] Thread for client '{client.name}' started.")
         except Exception as e:
-            print("[EXCEPTION]", e)
+            print("[AUTHENTICATION][ERROR]", e)
             conn.close()
 
     def player_input_thread(self, client):
         while True:
             try:
-                print('Server taken:', client.conn.recv(1024))
+                command = client.conn.recv(1024).decode()
+                # 0 - Send cmd to other clients
+                if command.startswith('0'):
+                    print('Command 0.')
+                    for cl in self.clients:
+                        if client != cl:
+                            cl.send(command[2::])
+                else:
+                    print('Invalid command:', command)
             except Exception as ex:
-                print('[ERROR]', ex)
+                print('[PLAYER THREAD ERROR] from:', client.name, ex)
+                self.clients.remove(client)
+                if self.waiting:
+                    self.connected -= 1
+                    print(f"[CONNECT] Disconnected [{self.connected}/{NEED_PLAYERS}]!")
+                return
 
 
 if __name__ == '__main__':
     server = Server()
     thread = threading.Thread(target=server.thread_connection)
     thread.start()
-    # thread.join()
-    while len(server.clients) == 0:
-        pass
-    server.send_all('0')
-    server.send_all('0')
-    server.send_all('1')
-    print('Incremented.')
+    thread.join()
+    print('Server closed.')
