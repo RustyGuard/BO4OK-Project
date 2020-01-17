29750cf90c6c1d813f47d9832c657851082d79fa
najisirotkin <najisirotkin@mail.com>
2020-01-17 20:52:13 +0400

    add window_setings

--- data.py
+++ data.py
@@ -9,10 +9,14 @@ nicname = ""
 
 
 class Button(pygame.sprite.Sprite):
-    def __init__(self, group, name, image, way):
+    def __init__(self, group, name, image, way, f=None):
         super().__init__(group)
-        self.stok_image = pygame.image.load(f'sprite-games/{way}/{name}.png')
-        self.anim = pygame.image.load(f'sprite-games/{way}/anim/{name}.png')
+        if not f:
+            self.stok_image = pygame.image.load(f'sprite-games/{way}/{name}.png')
+            self.anim = pygame.image.load(f'sprite-games/{way}/anim/{name}.png')
+        else:
+            self.stok_image = pygame.image.load(f'sprite-games/{way}/field.png')
+            self.anim = pygame.image.load(f'sprite-games/{way}/tick_field.png')
         self.name = name
         self.image = self.stok_image
         self.rect = self.image.get_rect()
@@ -24,6 +28,36 @@ class Button(pygame.sprite.Sprite):
         else:
             if self.image == self.anim:
                 self.image = self.stok_image
+
+
+def read_settings():
+    set = open('settings.txt', 'r')
+    settings = {}
+    for i in set.read().split("\n"):
+        a = i.split()
+        if a[1] == "TRUE":
+            settings[a[0]] = True
+        elif a[1] == "FALSE":
+            settings[a[0]] = False
+        else:
+            settings[a[0]] = a[1]
+    set.close()
+    return settings
+
+
+def write_settings(settings):
+    wr = []
+    set = open('settings.txt', 'w+')
+    for i in settings:
+        if settings[i] == True:
+            wr.append(i + " TRUE")
+        elif settings[i] == False:
+            wr.append(i + " FALSE")
+        else:
+            wr.append(i + " " + settings[i])
+    set.seek(0)
+    set.write("\n".join(wr))
+    set.close()
 
 
 def menu(screen):
@@ -54,7 +88,7 @@ def menu(screen):
                         if button.name == "play":
                             play(screen)
                         if button.name == "settings":
-                            pass
+                            settings(screen)
                         if button.name == "statistics":
                             pass
                         if button.name == "creators":
@@ -210,6 +244,55 @@ def play(screen):
         screen.blit(background, (0, 0))
         all_buttons.draw(screen)
         screen.blit(font.render(nicname, 1, (255, 255, 255)), (810, 740))
+        screen.blit(cursor, (pygame.mouse.get_pos()[0] - 9, pygame.mouse.get_pos()[1] - 5))
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def settings(screen):
+    global cursor, FPS, clock
+    background = pygame.image.load('sprite-games/settings/background.png')
+    screen.blit(background, (0, 0))
+    tick_field_image = {"BACKGROUND": (15, 183),
+                        "CAMERA": (15, 315)}
+    way = "settings"
+    settings = read_settings()
+
+    all_buttons = pygame.sprite.Group()
+    Button(all_buttons, "back", (12, 12), way)
+
+    all_tick_field = pygame.sprite.Group()
+    for i in tick_field_image:
+        Button(all_tick_field, i, tick_field_image[i], way, 1)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            if event.type == pygame.MOUSEMOTION:
+                for button in all_buttons:
+                    button.get_anim(event)
+                for tick_field in all_tick_field:
+                    tick_field.get_anim(event)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for button in all_buttons:
+                    if button.rect.collidepoint(event.pos):
+                        write_settings(settings)
+                        return
+                for tick_field in all_tick_field:
+                    if tick_field.rect.collidepoint(event.pos):
+                        if settings[tick_field.name]:
+                            settings[tick_field.name] = False
+                        else:
+                            settings[tick_field.name] = True
+        for tick_field in all_tick_field:
+            if settings[tick_field.name]:
+                tick_field.image = tick_field.anim
+            else:
+                tick_field.image = tick_field.stok_image
+        screen.blit(background, (0, 0))
+        all_buttons.draw(screen)
+        all_tick_field.draw(screen)
         screen.blit(cursor, (pygame.mouse.get_pos()[0] - 9, pygame.mouse.get_pos()[1] - 5))
         pygame.display.flip()
         clock.tick(FPS)
